9bf9ba1462e1310ab6b920ee1c066d87acde44f9
ceo-nasser <nasser5430966@gmail.com>
2025-02-16 15:14:18 +0000

    4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,16 +1,17 @@
-#include <stdio.h>
-
-
-/**
- *  main - prints "Programming is like building a multilingual
- *   puzzle"
- *   return 0.
+/*
+ * file 4puts
  */
 
+#include <stdio.h>
+
+/**
+ *  main - Prints Programming is like building a multilingual
+ *  puzzle
+ *  Return: Alawys 0
+ */
 int main(void)
+{
+	puts(":\"Programming is like building a multilingual puzzle");
 
-{	
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return(0);
-}	
+	return (0);
+}
