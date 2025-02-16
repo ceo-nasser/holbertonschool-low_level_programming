5b6bd9eae2812f2521afe71dff114b0f267d0bf1
ceo-nasser <nasser5430966@gmail.com>
2025-02-16 15:59:14 +0000

    6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 /**
-* main - Prints using printf() command
-* Return: 0 prints phrase
+* main - Prints the size of each variable
+* Return: 0 prints sizes
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("Size of a char: %ld byte(s)\n", sizeof(char));
+	printf("Size of an int: %ld byte(s)\n", sizeof(int));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
 }
