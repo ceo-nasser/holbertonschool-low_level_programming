a36819a516f3919313307d8d67cbc877039c8e0f
ceo-nasser <nasser5430966@gmail.com>
2025-02-19 10:27:37 +0000

    0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,5 +1,23 @@
-#include <stdio.h>
+#include "main.h"
 
-int main() {
-  printf("_putchar");
-  return 0;
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+
+}
