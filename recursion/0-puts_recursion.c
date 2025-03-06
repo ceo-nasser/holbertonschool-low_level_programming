c52c90591906c07dff7e033fef7fb38e1c94a868
ceo-nasser <nasser5430966@gmail.com>
2025-03-06 12:20:56 +0000

    holberton

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,39 +1,16 @@
 #include "main.h"
-
 /**
- *
- *  *_puts_recursion - prints a string
- *
- *   *@s: pointer block of memory to fill
- *
- *    *Return: void
- *
- *     */
-
-
-
+ * _puts_recursion - print like put
+ * @s: string
+ * Return: print string
+ */
 void _puts_recursion(char *s)
-
-
-
 {
-
 	if (*s == '\0')
-
-	{
-
 		_putchar('\n');
-
-		return;
-
+	else
+	{
+		_putchar(s[0]);
+		_puts_recursion(s + 1);
 	}
-
-
-
-	_putchar (*s);
-
-	_puts_recursion(s + 1);
-
-
-
 }
